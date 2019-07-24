af499b0dcb216a1f1806c1b3ef23d0037d27ef7b
Jack Conway <j124519951@gmail.com>
2019-07-24 12:26:31 -0400

    handle array

--- src/Pixie/QueryBuilder/NestedCriteria.php
+++ src/Pixie/QueryBuilder/NestedCriteria.php
@@ -12,6 +12,14 @@ class NestedCriteria extends QueryBuilderHandler
      */
     protected function whereHandler($key, $operator = null, $value = null, $joiner = 'AND')
     {
+        if(is_array($value)){
+            if($operator == 'NOT IN' || $operator == 'NOT'){
+                $operator = 'NOT IN';
+            }else{
+                $operator = 'IN';
+            }
+        }
+        
         $key = $this->addTablePrefix($key);
         $this->statements['criteria'][] = compact('key', 'operator', 'value', 'joiner');
         return $this;
--- src/Pixie/QueryBuilder/QueryBuilderHandler.php
+++ src/Pixie/QueryBuilder/QueryBuilderHandler.php
@@ -930,6 +930,14 @@ class QueryBuilderHandler
      */
     protected function whereHandler($key, $operator = null, $value = null, $joiner = 'AND')
     {
+        if(is_array($value)){
+            if($operator == 'NOT IN' || $operator == 'NOT'){
+                $operator = 'NOT IN';
+            }else{
+                $operator = 'IN';
+            }
+        }
+        
         $key = $this->addTablePrefix($key);
         $this->statements['wheres'][] = compact('key', 'operator', 'value', 'joiner');
         return $this;
